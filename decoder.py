5e7d110e48d5e4247a5267130f9eb7b8657045fa
jongwonjlee <jongwon5@illinois.edu>
2022-04-15 10:21:00 -0500

    split into modules

---
+++ decoder.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
+
+from vision_transformer import TransformerBlock
+
+# linear layer for semantic segmentation
+class DecoderLinear(nn.Module):
+    def __init__(self, n_cls, patch_size, d_encoder):
+        super().__init__()
+
+        self.d_encoder = d_encoder
+        self.patch_size = patch_size
+        self.n_cls = n_cls
+
+        self.head = nn.Linear(self.d_encoder, n_cls)
+        # self.apply(init_weights)
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return set()
+
+    def forward(self, x, im_size):
+        H, W = im_size
+        GS = H // self.patch_size
+        x = self.head(x)
+        x = rearrange(x, "b (h w) c -> b c h w", h=GS)
+
+        return x
+
+# mask trasformer in Segmenter
+class MaskTransformer(nn.Module):
+    def __init__(
+        self,
+        n_cls,
+        patch_size,
+        d_encoder,
+        n_layers,
+        n_heads,
+        d_model,
+        d_ff=2048,  # num of hidden features in mlp
+        drop_path_rate=0.0,
+        dropout=0.1,
+    ):
+        super().__init__()
+        self.d_encoder = d_encoder
+        self.patch_size = patch_size
+        self.n_layers = n_layers
+        self.n_cls = n_cls
+        self.d_model = d_model
+        self.d_ff = d_ff
+        self.scale = d_model ** -0.5
+
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, n_layers)]
+        self.transformer_block = TransformerBlock(embedding=d_encoder,
+                                            num_heads=n_heads,
+                                            hidden_features=d_ff,
+                                            qkv_b=True,
+                                            attention_dropout_p=0,
+                                            projection_dropout_p=0,
+                                            mlp_dropout_p=0)
+        self.blocks = nn.ModuleList(
+            [self.transformer_block for _ in range(n_layers)]
+        )
+
+        self.cls_emb = nn.Parameter(torch.randn(1, n_cls, d_model))
+        self.proj_dec = nn.Linear(d_encoder, d_model)
+
+        self.proj_patch = nn.Parameter(self.scale * torch.randn(d_model, d_model))
+        self.proj_classes = nn.Parameter(self.scale * torch.randn(d_model, d_model))
+
+        self.decoder_norm = nn.LayerNorm(d_model)
+        self.mask_norm = nn.LayerNorm(n_cls)
+
+        # self.apply(init_weights)
+        # trunc_normal_(self.cls_emb, std=0.02)
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {"cls_emb"}
+
+    def forward(self, x, im_size):
+        H, W = im_size
+        GS = H // self.patch_size
+
+        x = self.proj_dec(x)
+        cls_emb = self.cls_emb.expand(x.size(0), -1, -1)
+        x = torch.cat((x, cls_emb), 1)
+        for blk in self.blocks:
+            x = blk(x)
+        x = self.decoder_norm(x)
+
+        patches, cls_seg_feat = x[:, : -self.n_cls], x[:, -self.n_cls :]
+        patches = patches @ self.proj_patch
+        cls_seg_feat = cls_seg_feat @ self.proj_classes
+
+        patches = patches / patches.norm(dim=-1, keepdim=True)
+        cls_seg_feat = cls_seg_feat / cls_seg_feat.norm(dim=-1, keepdim=True)
+
+        masks = patches @ cls_seg_feat.transpose(1, 2)
+        masks = self.mask_norm(masks)
+        masks = rearrange(masks, "b (h w) n -> b n h w", h=int(GS))
+
+        return masks
